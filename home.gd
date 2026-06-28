36232347a1ce33f0a314e81371167c8e1bba9b1f
Shepard M <smullins232@gmail.com>
2026-06-28 13:03:59 -0400

    Finish

--- home.gd
+++ home.gd
@@ -7,19 +7,26 @@ var is_showing_result: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	display.text = "0"
-	# Dynamically connect all buttons inside the grid to one function
-	for button in self.get_children():
-		if button is Button:
-			button.pressed.connect(_on_button_pressed.bind(button.text))
+	_connect_buttons_recursively(self)
+
+func _connect_buttons_recursively(node: Node) -> void:
+	for child in node.get_children():
+		if child is Button:
+			(child as Button).pressed.connect(_on_button_pressed.bind((child as Button).text))
+		_connect_buttons_recursively(child)
 
 func _on_button_pressed(button_text: String) -> void:
-	match button_text:
-		"C":
+	var normalized_text: String = button_text.strip_edges().to_upper()
+
+	match normalized_text:
+		"C", "CLEAR":
 			_clear_calculator()
-		"=":
+		"=", "SOLVE":
 			_calculate_result()
-		"+", "-", "*", "/":
-			_add_operator(button_text)
+		"+", "-", "/":
+			_add_operator(normalized_text)
+		"*", "X":
+			_add_operator("*")
 		_:
 			_add_number(button_text)
 
